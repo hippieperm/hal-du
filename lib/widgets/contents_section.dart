9849bcc1acb294ad4445bb996bed7e6962f6ec2e
hippieperm <leess0133@gmail.com>
2025-09-15 08:59:53 +0900

    fix: 그림자 색상 투명도 값 수정

--- lib/widgets/contents_section.dart
+++ lib/widgets/contents_section.dart
@@ -139,7 +139,7 @@ class ContentsSection extends StatelessWidget {
         borderRadius: BorderRadius.circular(20),
         boxShadow: [
           BoxShadow(
-            color: Colors.grey.withOpacity(0.1),
+            color: Colors.grey.withValues(alpha: 0.1),
             blurRadius: 10,
             offset: const Offset(0, 5),
           ),
