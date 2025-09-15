a24839804deace2a2d8157e37569043f50c98996
hippieperm <leess0133@gmail.com>
2025-09-15 09:00:19 +0900

    fix: 그림자 색상 투명도 값 수정

--- lib/widgets/shop_section.dart
+++ lib/widgets/shop_section.dart
@@ -131,7 +131,7 @@ class ShopSection extends StatelessWidget {
         borderRadius: BorderRadius.circular(20),
         boxShadow: [
           BoxShadow(
-            color: Colors.grey.withOpacity(0.1),
+            color: Colors.grey.withValues(alpha: 0.1),
             blurRadius: 10,
             offset: const Offset(0, 5),
           ),
